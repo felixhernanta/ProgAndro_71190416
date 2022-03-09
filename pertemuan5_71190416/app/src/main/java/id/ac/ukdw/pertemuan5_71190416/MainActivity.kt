6ad060e595a760c5fde0859039843a1199e24f60
felixhernanta <felix.hernanta@ti.ukdw.ac.id>
2022-03-10 00:22:47 +0700

    Hasil1

--- pertemuan5_71190416/app/src/main/java/id/ac/ukdw/pertemuan5_71190416/MainActivity.kt
+++ pertemuan5_71190416/app/src/main/java/id/ac/ukdw/pertemuan5_71190416/MainActivity.kt
@@ -1,11 +1,23 @@
 package id.ac.ukdw.pertemuan5_71190416
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val ambilusername=intent.getStringExtra("username")
+        val cetakusername = findViewById<TextView>(R.id.getuser)
+        cetakusername.text="${ambilusername}"
+        val keluar=findViewById<Button>(R.id.buttonlogout)
+        keluar.setOnClickListener{
+            val i=Intent(this, LoginActivity::class.java)
+            startActivity(i)
+            finish()
+        }
     }
 }
