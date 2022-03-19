eb12f69cf0312528db984f5971b09dd6e8f646d2
felixhernanta <felix.hernanta@ti.ukdw.ac.id>
2022-03-20 01:37:25 +0700

    pert_6

---
+++ pertemuan6_71190416/app/src/main/java/id/ac/ukdw/pertemuan6_71190416/FragmentA.kt
@@ -0,0 +1,19 @@
+package id.ac.ukdw.pertemuan6_71190416
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import android.widget.Toast
+import androidx.fragment.app.Fragment
+
+class FragmentA : Fragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_a,container,false)
+    }
+}
